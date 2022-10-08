bc8c0b8e35f69cf3d7e65e42aee3f8d588666076
bogdan-brz <bogdan.brzuchacz@gmail.com>
2022-10-08 09:36:25 -0700

    all done, will experiment with styling now

--- components/pages/contact/ContactPage.js
+++ components/pages/contact/ContactPage.js
@@ -1,7 +1,24 @@
 import styles from "./ContactPage.module.css";
 
 const ContactPage = () => {
-    return <div className={"page"}>Contact Page</div>;
+    return (
+        <div className={"page " + styles.contact}>
+            <div className={styles.content}>
+                <h1>Contact Info</h1>
+                <div className={styles.info}>
+                    email: <span>contactinfo@domain.com</span>
+                </div>
+                <div className={styles.info}>
+                    phone number: <span>###-###-####</span>
+                </div>
+                <p>
+                    We usually respond to emails within a couple hours even
+                    outside of our open hours, but we take phone calls only
+                    within open hours.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default ContactPage;
